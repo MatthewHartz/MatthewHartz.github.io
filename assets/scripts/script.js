8434674c5ad948a604e99a891cd2d5c2c81ab795
rvbmrdonut <hartzm@uci.edu>
2015-10-21 21:15:16 -0700

    Reformatted CSS
    Fixed issue of weird header
    Added flipper to my mini image

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,23 @@
 $(function() {
-    $("#navibar").on('affixed.bs.affix', function(){
+	// Used because if the user is at top position 100 (when bar becomes sticky)
+	// they click to fire both events
+	POSITION = 0;
+	
+	$("#navibar").on('affixed.bs.affix', function(){
         //alert("The navigation menu has been affixed. Now it doesn't scroll with the page.");
+		var temp = $(window).scrollTop();
+		if (temp != POSITION) {
+			$("#stickyImage").show();
+		}
+		POSITION = temp
     });
 	
 	$("#navibar").on('affixed-top.bs.affix', function(){
         //alert("The navigation menu has been affixed. Now it doesn't scroll with the page.");
+		var temp = $(window).scrollTop();
+		if (temp != POSITION) {
+			$("#stickyImage").hide();
+		}
+		POSITION = temp
     });
 });
