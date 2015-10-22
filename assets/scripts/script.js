05e7835630e7fe46ac72e93d8257ce36542923c4
rvbmrdonut <hartzm@uci.edu>
2015-10-21 20:05:42 -0700

    Save point
    Added disqus (might not be 100%)
    Made sticky header
    Save point because of css changes.

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#navibar").on('affixed.bs.affix', function(){
+        //alert("The navigation menu has been affixed. Now it doesn't scroll with the page.");
+    });
+	
+	$("#navibar").on('affixed-top.bs.affix', function(){
+        //alert("The navigation menu has been affixed. Now it doesn't scroll with the page.");
+    });
+});
